8cc5979ecd0286b1cc84adbf3ae9556366f2a1d8
justingreco <justin.greco@gmail.com>
2023-04-12 09:07:43 -0400

    Initial Commit

---
+++ imaps-mobile/imaps_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  imaps_mobileApp.swift
+//  imaps-mobile
+//
+//  Created by Greco, Justin on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct imaps_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
